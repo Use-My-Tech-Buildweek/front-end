13f99d9b1431dbda71a4f549c617f85707594788
Greg Brewton <gbrewton1980@gmail.com>
2021-06-16 20:22:34 -0600

    Fixes typo in Welcome.js <form> element

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,31 +1,30 @@
-import Item from './Item'
+import Item from "./Item";
 
 const Welcome = () => {
-    return(
-        <>
-        <from>
-            <label>
-                I'm looking for :
-                <input type='text' placeholder="I'm looking for...."/>
-            </label>
-           
-        </from>
-        <div>
-            <h2>Electronics</h2>
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-        </div>
-        <div>
-            <h2>Tools / DIY</h2>
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <form>
+        <label>
+          I'm looking for :
+          <input type="text" placeholder="I'm looking for...." />
+        </label>
+      </form>
+      <div>
+        <h2>Electronics</h2>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </div>
+      <div>
+        <h2>Tools / DIY</h2>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </div>
+    </>
+  );
+};
 
-export default Welcome
+export default Welcome;
