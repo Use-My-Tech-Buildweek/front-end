40c4c0f311c819b258a7e175651ea18484bcace6
Greg Brewton <gbrewton1980@gmail.com>
2021-06-18 20:42:04 -0600

    Completes signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,8 +7,9 @@ const Signup = () => {
     role: "",
     email: "",
     password: "",
-    aboutMe: "",
-    profilePicture: null,
+    pw_validate: "",
+    about_me: "",
+    profilePicture: "",
   };
   const [formValues, setFormValues] = useState(initialFormValues);
 
@@ -17,19 +18,42 @@ const Signup = () => {
     M.FormSelect.init(elems);
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: submit form values to backend and reset form
+  };
+
+  const handlechange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    console.log(name, value);
+    setFormValues({ ...formValues, [name]: value });
+  };
+
   return (
     // pass a prop to specify if the submit must create a new user or update one .. (see if user id not undefined)
     <div className="row">
-      <form action="submit" className="col s12">
+      <form action="submit" className="col s12" onSubmit={handleSubmit}>
         <div className="row">
           <div className="input-field col s6">
-            <input name="username" id="username" type="text" />
+            <input
+              name="username"
+              id="username"
+              type="text"
+              onChange={handlechange}
+              value={formValues.username}
+            />
             <label htmlFor="username">Username</label>
           </div>
         </div>
         <div className="row">
           <div className="input-field col s12">
-            <select name="role" id="role">
+            <select
+              name="role"
+              id="role"
+              onChange={handlechange}
+              value={formValues.role}
+            >
               <option value="" disabled default>
                 Choose your role
               </option>
@@ -45,17 +69,35 @@ const Signup = () => {
         </div>
         <div className="row">
           <div className="input-field col s6">
-            <input type="email" id="email" name="email" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={handlechange}
+              value={formValues.email}
+            />
             <label htmlFor="email">Email</label>
           </div>
         </div>
         <div className="row">
           <div className="input-field col s6">
-            <input type="password" name="password" id="password" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={handlechange}
+              value={formValues.password}
+            />
             <label htmlFor="password"></label>
           </div>
           <div className="input-field col s6">
-            <input type="password" name="pw_verify" id="pw_verify" />
+            <input
+              type="password"
+              name="pw_validate"
+              id="pw_validate"
+              onChange={handlechange}
+              value={formValues.pw_validate}
+            />
             <label htmlFor="pw_verify"></label>
           </div>
         </div>
@@ -66,13 +108,33 @@ const Signup = () => {
               id="about_me"
               cols="30"
               rows="10"
-            ></textarea>
+              className="materialize-textarea"
+              onChange={handlechange}
+              value={formValues.aboutMe}
+            />
             <label htmlFor="about_me">Introduce Yourself</label>
           </div>
         </div>
         <div className="row">
-          <div className="input-field col s6">
-            <input name="profilePicture" type="file" accept=".jpg,.jpeg,.png" />
+          <div className="file-field input-field col s12">
+            <div className="btn">
+              <span>Profile Picture</span>
+              <input
+                type="file"
+                name="profilePicture"
+                onChange={handlechange}
+                value={formValues.profilePicture}
+              />
+            </div>
+            <div className="file-path-wrapper">
+              <input
+                className="file-path validate"
+                name="pw_validate"
+                type="text"
+                onChange={handlechange}
+                value={formValues.pw_validate}
+              />
+            </div>
           </div>
         </div>
         <div className="row">
@@ -84,43 +146,6 @@ const Signup = () => {
         </div>
       </form>
     </div>
-    // <form>
-
-    //     <label>
-    //         Username
-    //         <input name="username" type="text" placeholder="choose your username"/>
-    //     </label>
-    //     <label>
-    //         Role
-    //         <select name="role">
-    //             <option value="" selected disabled hidden>== option ==</option>
-    //             <option value="Renter">Renter</option>
-    //             <option value="Client">Client</option>
-    //         </select>
-    //     </label>
-    //     <label>
-    //         email
-    //         <input name="email" type="text" placeholder="contact email"/>
-    //     </label>
-    //     <label>
-    //         Password
-    //         <input name="password" type="text" placeholder="enter your password"/>
-    //     </label>
-    //     <label>
-    //         Confirm your Password
-    //         <input name="passwordConfirmation" type="text" placeholder="confirm your password"/>
-    //     </label>
-    //     <label>
-    //         introduce yourself briefly
-    //         <textarea name="aboutMe" type="text" placeholder="about me..."/>
-    //     </label>
-    //     <label>
-    //         Profile picture
-    //         <input name="profilePicture" type="file" accept=".jpg,.jpeg,.png" placeholder="about me..."/>
-    //     </label>
-    //     <button type="input">Sign up</button>
-
-    // </form>
   );
 };
 
