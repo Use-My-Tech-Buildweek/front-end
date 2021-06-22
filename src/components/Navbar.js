802792505fc0f50a7b78946b6f81e27231df64d5
Joe Hebert <raphaelhebert18@gmail.com>
2021-06-22 14:42:26 +0700

    added logout button and modal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,42 @@ import M from "materialize-css";
 import { navButtonStyle } from "./styles/styles";
 
 const Navbar = () => {
+  
+  //=============== logout function and modal =========================
+  //functions
+
+  const triggerModal = () => {
+    const modal = document.getElementById('logOutModal');
+    modal.style.display = "block";
+    modal.style.position = "absolute";
+    modal.style.top = "10%";
+    modal.style.left = "40%";
+  }
+
+  const logOutFromModal = () => {
+    // call to logout function
+    const modal = document.getElementById('logOutModal');
+    modal.style.display = "none";
+  }
+
+  const cancelModal = () => {
+    const modal = document.getElementById('logOutModal');
+    modal.style.display = "none";
+  }
+
+  //add button to dom 
+  useEffect(() =>{
+    // modal styling
+    const modal = document.getElementById('logOutModal');
+    modal.style.display = "none";
+    modal.style.background = "white";
+    modal.style.border = "3px solid red";
+    modal.style.borderRadius = "10px";
+    modal.style.padding = "4%"; 
+    modal.style.zIndex = 10;
+  }, [])
+  // =====================================================================
+
   const [location, setLocation] = useState("/");
 
   //   TODO: disable the navigation link for the current page
@@ -22,6 +58,17 @@ const Navbar = () => {
   }, [path, location]);
 
   return (
+    <>
+    {/* Modal */}
+    <div id="logOutModal">
+      <p>Are you sure you want to log out?</p>
+      <div>
+        <button onClick={() => {logOutFromModal()}}>Log me out!</button>
+        <button onClick={cancelModal}>Cancel</button>
+      </div>
+    </div>
+   {/* end of modal */}
+
     <div className="nav-wrapper">
       <nav style={{ padding: "0 1em" }} className="valign-wrapper">
         <a href="#" data-target="responsive-nav" className="sidenav-trigger">
@@ -50,6 +97,17 @@ const Navbar = () => {
               <Link to="/login">Log In</Link>
             </span>
           </button>
+          {/* to display if user is logged in */}
+          <button
+            style={navButtonStyle}
+            className="right waves-effect-light btn"
+            id="logOutButton"
+            onClick={() => {triggerModal()}}
+          >
+            <span className="valign-wrapper">
+              Log Out
+            </span>
+          </button>
         </div>
       </nav>
       <ul className="sidenav" id="responsive-nav">
@@ -58,6 +116,7 @@ const Navbar = () => {
         <li><Link to="/login" >Log In</Link></li>
       </ul>
     </div>
+    </>
   );
 };
 
