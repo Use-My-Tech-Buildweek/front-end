51193b8f07e8493d9af45da04b050ccb325f5e17
Greg Brewton <gbrewton1980@gmail.com>
2021-06-22 19:58:38 -0600

    Adds text counters to SignupForm, finishes styling

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,177 +1,199 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useState, useEffect } from "react";
+// eslint-disable-next-line no-unused-vars
+import { connect } from "react-redux";
 import M from "materialize-css";
+// eslint-disable-next-line no-unused-vars
+import { addUser, setError } from "../actions/userActions";
+import { useHistory } from "react-router-dom";
+import { searchWrapperStyle } from "./styles/styles";
 
-import { addUser, setError } from '../actions/userActions'
+const SignupForm = (props) => {
+  let history = useHistory();
+  //   const { visible, toggleVisible } = props;
+  const initialFormValues = {
+    username: "",
+    password: "",
+    role: "default",
+    email: "",
+    pw_validate: "",
+    about_me: "",
+    profile_picture: "",
+    department: "",
+    error: "",
+    isFilePicked: false,
+    id: "",
+  };
 
-class SignupForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            newUser: {
-                username: '',
-                password: '',
-                // email: '',
-                //bio,
-                //profileImg: '',
-                department: '',
-            },
-            error: '',
-            isFilePicked: false
-        }
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  useEffect(() => {
+    const elems = document.querySelectorAll("select");
+    M.FormSelect.init(elems);
+
+    // TODO: add stronger checks, such as file type and size
+    if (
+      formValues.profile_picture !== "" &&
+      formValues.isFilePicked === false
+    ) {
+      setFormValues({ ...formValues, isFilePicked: true });
     }
-    componentDidMount() {
-        const elems = document.querySelectorAll("select");
-        M.FormSelect.init(elems);
-    }
+  }, [formValues]);
 
-    handleSubmit = e => {
-        e.preventDefault()
-        console.log('submit add new user button clicked, calling addUser', this.state.newUser)
-        this.props.addUser(this.state.newUser)
-        this.props.history.push(`/profile/:${this.props.user.id}`);
-    }
+  useEffect(() => {
+    const counters = document.querySelectorAll(".counter_input");
+    M.CharacterCounter.init(counters);
+  }, []);
 
-    handleChanges = e => {
-        this.setState({
-            newUser: {
-                ...this.state.newUser,
-                [e.target.name]: e.target.value
-            }
-        })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(
+      "submit add new user button clicked, calling addUser",
+      formValues
+    );
+    // Submit form values to backend and reset form
+    addUser(formValues); //should be sending identical data as "this.props.addUser(this.state.newUser)"
+    // Clear form
+    setFormValues(initialFormValues);
+    // Programmatic redirect to profile page
+    history.push(`/profile/:${formValues.id}`);
+  };
 
-    handleSelectFile = e => {
-        this.setState({
-            ...this.state.user,
-            profileImg: e.target.files[0],
-        })
-        this.setState({
-            ...this.state,
-            isFilePicked: true,
-        })
-    }
-    render() {
-        return (
-            <div className='row'>
-                <form action='submit' className='col s12' onSubmit={this.handleSubmit}>
-                    <div className='row'>
-                        <div className='input-field col s6'>
-                            <input
-                                name="username"
-                                type="text"
-                                id='username'
-                                autoComplete='username'
-                                onChange={this.handleChanges}
-                                value={this.state.newUser.username}
-                            />
-                            <label htmlFor="username">Username</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <select
-                                name="department"
-                                id="department"
-                                onChange={this.handleChanges}
-                                value={this.state.newUser.department}
-                            >
-                                <option name="department" value="default" >
-                                    Choose your role
-                        </option>
-                                <option name="department" value="renter">
-                                    Renter
-                        </option>
-                                <option name="department" value="owner">
-                                    Owner
-                        </option>
-                            </select>
-                            <label htmlFor="department">Account Type</label>
-                        </div>
-                    </div>
-                    {/*  <div className="row">
-                        <div className="input-field col s6">
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                onChange={this.handleChanges}
-                                value={this.state.newUser.email}
-                            />
-                            <label htmlFor="email">Email</label>
-                        </div>
-                    </div>
-        */}
-                    <div className="row">
-                        <div className="input-field col s6">
-                            <input
-                                type="password"
-                                name="password"
-                                id="password"
-                                autoComplete='current-password'
-                                onChange={this.handleChanges}
-                                value={this.state.newUser.password}
-                            />
-                            <label htmlFor="password"></label>
-                        </div>
-                        {/*<div className="input-field col s6">
-                            <input
-                                type="password"
-                                name="pw_validate"
-                                id="pw_validate"
-                                onChange={this.handleChanges}
-                                value={this.state.pw_validate}
-                            />
-                            <label htmlFor="pw_verify"></label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <textarea
-                                name="bio"
-                                id="bio"
-                                cols="30"
-                                rows="10"
-                                className="materialize-textarea"
-                                onChange={this.handleChanges}
-                                value={this.state.newUser.bio}
-                            />
-                            <label htmlFor="bio">Introduce Yourself</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        {/*    <div className="file-field input-field col s12">
-                               <div className="btn">
-                              <span>Profile Picture</span>
-                                <input
-                                 type="file"
-                                name="profile_picture"
-                          onChange={handlechange}
-                             value={formValues.profile_picture}
-                                 />
-                                   </div>
-                            </div>*/}
-                        <div className="row">
-                            <div className="col s6">
-                                <button type="submit" className="btn btn-waves-effect">
-                                    Submit
-                            </button>
-                            </div>
-                        </div>
+  const handlechange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    console.log(name, value);
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-
-                    </div>
-                </form>
+  return (
+    // pass a prop to specify if the submit must create a new user or update one .. (see if user id not undefined)
+    // TODO: Add text counters
+    // TODO: Add field validation
+    // Integrate Sarah's changes (Redux)
+    <div className="container">
+      <div className="row">
+        <form action="submit" className="col s12" onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="input-field col s6">
+              <input
+                name="username"
+                className="counter_input"
+                maxLength="21"
+                data-length="20"
+                id="username"
+                type="text"
+                onChange={handlechange}
+                value={formValues.username}
+              />
+              <label htmlFor="username">Username</label>
             </div>
-        )
-    }
-}
-const mapStateToProps = state => {
-    return {
-        users: state.users,
-        newUser: state.newUser,
-        error: state.error,
-        sendApiCall: state.sendApiCall,
-    }
-}
-export default connect(mapStateToProps, { setError, addUser })(SignupForm)
+            <div className="input-field col s6">
+              <input
+                className="counter_input"
+                maxLength="31"
+                data-length="30"
+                type="email"
+                id="email"
+                name="email"
+                onChange={handlechange}
+                value={formValues.email}
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="input-field col s12">
+              <select
+                name="role"
+                id="role"
+                onChange={handlechange}
+                value={formValues.role}
+              >
+                <option name="role" value="default" disabled>
+                  Choose your role
+                </option>
+                <option name="role" value="renter">
+                  Renter
+                </option>
+                <option name="role" value="client">
+                  Client
+                </option>
+              </select>
+              <label htmlFor="role">Role</label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="input-field col s12">
+              <textarea
+                maxLength="301"
+                data-length="300"
+                name="about_me"
+                id="about_me"
+                cols="30"
+                rows="10"
+                className="materialize-textarea counter_input"
+                onChange={handlechange}
+                value={formValues.about_me}
+              />
+              <label htmlFor="about_me">Introduce Yourself</label>
+            </div>
+          </div>
+
+          <div className="row">
+            {/* I got the upload working, you can pick a file on your computer and it shows a fakepath. */}
+            <div className="file-field input-field col s12">
+              <div className="btn">
+                <label htmlFor="profile_picture" className="active white-text">
+                  Profile Picture
+                </label>
+                <input
+                  type="file"
+                  id="profile_picture"
+                  name="profile_picture"
+                  accept="image/png, image/jpeg"
+                  onChange={handlechange}
+                  value={formValues.profile_picture}
+                />
+              </div>
+              <div className="file-path-wrapper">
+                <input
+                  type="text"
+                  className="file-path counter_input"
+                  value={formValues.profile_picture}
+                  onChange={handlechange}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div
+              className="input-field col s6 offset-s3"
+              style={searchWrapperStyle}
+            >
+              <input
+                type="submit"
+                value="submit"
+                className="white-text waves-effect waves-light"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+// const mapStateToProps = (user) => {
+//   return {
+//     users: users,
+//     newUser: formValues,
+//     error: error,
+//     sendApiCall: sendApiCall,
+//   };
+// };
+
+// export default connect(mapStateToProps, { setError, addUser })(SignupForm);
+export default SignupForm;
---
+++ src/components/temp_copy_of_SignupForm.js
@@ -0,0 +1,183 @@
+import React from "react";
+import { connect } from "react-redux";
+import M from "materialize-css";
+
+import { addUser, setError } from "../actions/userActions";
+
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newUser: {
+        username: "",
+        password: "",
+        // email: '',
+        //bio,
+        //profileImg: '',
+        department: "",
+      },
+      error: "",
+      isFilePicked: false,
+    };
+  }
+  componentDidMount() {
+    const elems = document.querySelectorAll("select");
+    M.FormSelect.init(elems);
+  } // GTG
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(
+      "submit add new user button clicked, calling addUser",
+      this.state.newUser
+    );
+    this.props.addUser(this.state.newUser);
+    /** 
+    Where is she getting user.id from?
+    The only props passed in are visible and setVisible
+    I'm guessing this takes you to your profile on submission
+    */
+    this.props.history.push(`/profile/:${this.props.user.id}`);
+  };
+
+  handleChanges = (e) => {
+    this.setState({
+      newUser: {
+        ...this.state.newUser,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };// G2G
+
+  handleSelectFile = (e) => {
+    this.setState({
+      ...this.state.user,
+      profileImg: e.target.files[0],
+    });
+    this.setState({
+      ...this.state,
+      isFilePicked: true,
+    });
+  };
+  render() {
+    return (
+      <div className="row">
+        <form action="submit" className="col s12" onSubmit={this.handleSubmit}>
+          <div className="row">
+            <div className="input-field col s6">
+              <input
+                name="username"
+                type="text"
+                id="username"
+                autoComplete="username"
+                onChange={this.handleChanges}
+                value={this.state.newUser.username}
+              />
+              <label htmlFor="username">Username</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <select
+                name="department"
+                id="department"
+                onChange={this.handleChanges}
+                value={this.state.newUser.department}
+              >
+                <option name="department" value="default">
+                  Choose your role
+                </option>
+                <option name="department" value="renter">
+                  Renter
+                </option>
+                <option name="department" value="owner">
+                  Owner
+                </option>
+              </select>
+              <label htmlFor="department">Account Type</label>
+            </div>
+          </div>
+          {/*  <div className="row">
+                        <div className="input-field col s6">
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                onChange={this.handleChanges}
+                                value={this.state.newUser.email}
+                            />
+                            <label htmlFor="email">Email</label>
+                        </div>
+                    </div>
+        */}
+          <div className="row">
+            <div className="input-field col s6">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={this.handleChanges}
+                value={this.state.newUser.password}
+              />
+              <label htmlFor="password"></label>
+            </div>
+            {/*<div className="input-field col s6">
+                            <input
+                                type="password"
+                                name="pw_validate"
+                                id="pw_validate"
+                                onChange={this.handleChanges}
+                                value={this.state.pw_validate}
+                            />
+                            <label htmlFor="pw_verify"></label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <textarea
+                                name="bio"
+                                id="bio"
+                                cols="30"
+                                rows="10"
+                                className="materialize-textarea"
+                                onChange={this.handleChanges}
+                                value={this.state.newUser.bio}
+                            />
+                            <label htmlFor="bio">Introduce Yourself</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        {/*    <div className="file-field input-field col s12">
+                               <div className="btn">
+                              <span>Profile Picture</span>
+                                <input
+                                 type="file"
+                                name="profile_picture"
+                          onChange={handlechange}
+                             value={formValues.profile_picture}
+                                 />
+                                   </div>
+                            </div>*/}
+            <div className="row">
+              <div className="col s6">
+                <button type="submit" className="btn btn-waves-effect">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+    newUser: state.newUser,
+    error: state.error,
+    sendApiCall: state.sendApiCall,
+  };
+};
+export default connect(mapStateToProps, { setError, addUser })(SignupForm);
