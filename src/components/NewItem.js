94af3d42d3c452eabfdbb7cda88424a9f8a8f1f5
Sarah Guidry <sarahbethguidry@gmail.com>
2021-06-27 20:43:15 -0500

    add necessary fields to add item

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -1,37 +1,101 @@
+import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import { connect } from 'react-redux'
+import { addNewItem } from '../actions/itemsActions'
 
 
-const NewItem = () => {
+const NewItem = props => {
+
   const history = useHistory();
+  const [newItem, setNewItem] = useState({
+    availability: 0,
+    condition: '',
+    daily_rate: '',
+    description: '',
+    imgs: '',
+    item_name: '',
+    location: '',
+    user_id: props.user.id,
+  })
+
+  const changeHandler = e => {
+    setNewItem({
+      ...newItem,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const onAdd = e => {
+    e.preventDefault()
+    props.addNewItem(newItem.user_id, newItem)
+  }
 
   return (
     <>
       <h2>Add a new item</h2>
-      <form>
+      <form onSubmit={onAdd}>
         <label>
           Item name
-          <input name="itemName" type="text" />
+          <input
+            name="item_name"
+            type="text"
+            onChange={changeHandler}
+          />
         </label>
         <label>
           Description
-          <textarea name="itemDescription" />
+          <textarea
+            name="description"
+            type='text'
+            onChange={changeHandler}
+          />
         </label>
+        <div className='row'>
+          <label>
+            Number available
+            <input
+              name='availability'
+              type='number'
+              onChange={changeHandler}
+            />
+          </label>
+          <label>Daily Rate <input type='number' name='daily_rate' /></label>
+          <label>
+            Price
+            <input
+              name="itemPrice"
+              type="number"
+              onChange={changeHandler}
+            />
+          </label>
+        </div>
         <label>
-          Price
-          <input name="itemPrice" type="number" />
+          Condition
+          <input
+            type='text'
+            name='condition'
+            onChange={changeHandler}
+          />
+        </label>
+        <label>Location
+          <input
+            type='text'
+            name='location'
+            onChange={changeHandler} />
         </label>
         <label>
           Pictures
           <input
-            name="itemPictures"
+            name="imgs"
             type="file"
             accept=".jpg,.jpeg,.png"
             placeholder="item pictures..."
+            onChange={changeHandler}
             multiple
           />
         </label>
         <button>Add this Item</button>
+
       </form>
       <button onClick={() => history.goBack()}>Cancel</button>
     </>
@@ -41,7 +105,8 @@ const mapStateToProps = state => {
   return {
     myItems: state.users.myItems,
     items: state.items.items,
-    itemList: state.itemList.itemList
+    itemList: state.itemList.itemList,
+    user: state.users.user
   }
 }
-export default connect(mapStateToProps, {})(NewItem)
+export default connect(mapStateToProps, { addNewItem })(NewItem)
